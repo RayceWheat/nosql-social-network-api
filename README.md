acc0f57d3d9d55e173c49b858a8438f682e8b435
RayceWheat <76971187+RayceWheat@users.noreply.github.com>
2021-12-14 05:48:13 -0500

    Final readme update

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # nosql-social-network-api
+![](https://img.shields.io/badge/license-MIT-blue)
+
+## Description 
+This is a nosql backend using  moment, mongoose and express. 
+
+## Installation
+Once cloned used 'npm install' to download the dependacies.
+
+## Usage
+There is no front end so once 'npm start' is called something like Insomina is needed to interact with it.
+
+## Videolink
+![Video](https://github.com/RayceWheat/nosql-social-network-api/blob/develop/public/assets/video/demo%20video.mp4)
+
+## License
+Licensed under the [MIT License](LICENSE)
+
+## Credits 
+This code was written by Rayce Wheat
---
+++ controllers/user-controller.js
@@ -0,0 +1,97 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+    // get all users 
+    getAllUser(req, res) {
+        User.find({})
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+          })
+        .populate({
+            path: 'friends',
+            select: '-__v'
+        })
+        .select('-__v')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
+    // get one user by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    }, 
+
+
+    // Create User
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.json(err));
+    },
+
+    // update user by id 
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
+
+    // Delete User
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.json(err));
+    },
+
+    // add friend
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $addToSet: { friends: params.friendId } },
+            { new: true }
+        )
+        // .then(({ _id }) =>  {
+        // return User.findOneAndUpdate(
+        //     {id: params.userId },
+        //     { $push: { friends: _id } },
+        //     { new: true }
+        // );
+   
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user find with this id!' })
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => res.json(err));
+    },
+
+    // deelete from friend list
+    removeFriend({ params}, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: { friends: params.friendId } },
+            { new: true}
+        )
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.json(err));
+    }
+}
+
+module.exports = userController;
